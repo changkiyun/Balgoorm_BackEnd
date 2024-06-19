16e7b4f8da2f9a2169f00cd8dbe9d1292080cb4c
Junyoung Kim <zvpdrnls@gmail.com>
2024-06-19 18:00:54 +0900

    [BE] 시큐리티 수정

--- src/main/java/com/balgoorm/balgoorm_backend/user/config/SecurityConfig.java
+++ src/main/java/com/balgoorm/balgoorm_backend/user/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.balgoorm.balgoorm_backend.user.config;
 
 import com.balgoorm.balgoorm_backend.user.auth.CustomAuthenticationSuccessHandler;
 import com.balgoorm.balgoorm_backend.user.auth.CustomUserDetailsService;
+import com.balgoorm.balgoorm_backend.user.auth.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,51 +17,21 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
+
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
     private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+    }
+
     @Bean
-
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers( "/css/", "/js/", "/img/", "/font/").permitAll()
-//                        .requestMatchers("/admin/").hasRole("ADMIN")
-//                        .requestMatchers("/member/", "/board/**").hasRole("USER")
-//                        .requestMatchers("/login","/signup").anonymous()
-                        .anyRequest().permitAll()
-                )
-                .cors(cors -> cors.disable())
-                .csrf(csrf -> csrf.disable())
-                .formLogin(formLogin -> formLogin.disable())
-                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 비활성화; // 간단히 하기 위해 CSRF 비활성화. 실제 운영 환경에서는 활성화 고려
-                .formLogin(login -> {
-                    login.loginPage("/login.html") // 로그인 폼
-                            .loginProcessingUrl("/login") // 로그인 폼이 제출될 URL 설정
-                            .successHandler(customAuthenticationSuccessHandler) // 로그인 성공 시 커스텀 핸들러
-                            .failureUrl("/login.html?error=true") // 실패했을 때 리턴하는 주소
-                            .usernameParameter("userId") // 해당 메소드에 적은 userId를 login form에 적힌 userId와 일치 시킴
-                            .passwordParameter("password") // 비밀번호 파라미터 설정
-                            .permitAll();
-                })
-                .logout(logout -> {
-                    logout.logoutUrl("/logout")
-                            .logoutSuccessUrl("/login.html?logout")
-                            .invalidateHttpSession(true)
-                            .deleteCookies("JSESSIONID")
-                            .permitAll()
-                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")); // 로그아웃을 GET 방식으로 처리
-                })
-                .sessionManagement(session -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                            .maximumSessions(1)
-                            .maxSessionsPreventsLogin(false);
-                })
-                .userDetailsService(customUserDetailsService);
-
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
