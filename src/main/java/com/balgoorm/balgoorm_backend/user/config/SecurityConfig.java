6cd363109934b5ac1804b0caf8b984c5a0091e64
Junyoung Kim <zvpdrnls@gmail.com>
2024-06-19 18:20:41 +0900

    [BE] 시큐리티 설정 수정

--- src/main/java/com/balgoorm/balgoorm_backend/user/config/SecurityConfig.java
+++ src/main/java/com/balgoorm/balgoorm_backend/user/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.balgoorm.balgoorm_backend.user.config;
 
 import com.balgoorm.balgoorm_backend.user.auth.CustomAuthenticationSuccessHandler;
 import com.balgoorm.balgoorm_backend.user.auth.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +16,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
